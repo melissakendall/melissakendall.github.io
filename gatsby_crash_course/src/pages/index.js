3117838492fb558fbc28bb52f3a6bb81558f6c8f
Melissa Kendall <melissak@iqmetrix.com>
2019-07-16 12:41:59 -0600

    gatsby tutorial

---
+++ gatsby_crash_course/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to my website</h1>
+    <p>This is a sample site for the Gatsby crash course</p>
+  </div>
+)
+
+export default IndexPage
